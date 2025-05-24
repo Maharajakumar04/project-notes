9558e0578676909b33ec609c1a9d1f51f625773c
Maharajakumar04 <163717370+Maharajakumar04@users.noreply.github.com>
2025-05-24 23:46:02 +0530

    Repo is updated

--- src/layouts/DashboardLayouts.jsx
+++ src/layouts/DashboardLayouts.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet , useLocation} from "react-router";
+import { Link, Outlet, useLocation } from "react-router";
 
 const PATHS = [
     { path: '/dashboard/intro', text: 'intro' },
@@ -8,12 +8,13 @@ const PATHS = [
     { path: '/dashboard/profile', text: 'profile' },
     { path: '/dashboard/settings', text: 'settings' },
 ];
+
 export default function DashboardLayouts() {
     const location = useLocation();
     console.log(location.pathname);
     return (
         <div className="text-white flex min-h-screen">
-            <aside className="p-10 w-[200px] sticky top-0 h-screen bg-black flex flex-col item-center justify-start shadow-lg">
+            <aside className="p-10 w-[200px] sticky top-0 h-screen bg-black flex flex-col items-center justify-start shadow-lg">
                 <span className="text-2xl font-bold mb-4">Dashboard</span>
                 <nav className="space-y-4">
 
@@ -24,13 +25,15 @@ export default function DashboardLayouts() {
                                     key={index}
                                     to={value.path}
                                     style={location.pathname === value.path ? STYLE.active : STYLE.inactive}
-                                    className="block px-4 rounded hover:underline hover:text-white transition"
+                                    className="block px-4  rounded hover:underline hover:text-white transition"
                                 >
                                     {value.text}
                                 </Link>
                             )
                         })
                     }
+
+
                 </nav>
             </aside>
             <main className="min-h-screen overflow-auto text-black">
